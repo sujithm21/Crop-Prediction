284a7c165c4851899af413977a20ff9d5509e0d9
sujithm21 <makamsujith2004@gmail.com>
2024-04-28 12:56:16 +0530

    Xb 100% accuracy and app is working

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 import numpy as np
-import pandas
-import sklearn
+import pandas as pd
 import pickle
 
-# importing model
-model = pickle.load(open('models/RandomForest.pkl', 'rb'))
-#sc = pickle.load(open('standscaler.pkl', 'rb'))
-#ms = pickle.load(open('minmaxscaler.pkl', 'rb'))
+# Load the trained model
+model = pickle.load(open('models/XGBoost.pkl', 'rb'))
 
-# creating flask app
+# Define the crop dictionary
+crop_indices = {
+    "Soyabeans": 0,
+    "Apple": 1,
+    "Banana": 2,
+    "Beans": 3,
+    "Coffee": 4,
+    "Cotton": 5,
+    "Cowpeas": 6,
+    "Grapes": 7,
+    "Groundnuts": 8,
+    "Maize": 9,
+    "Mango": 10,
+    "Orange": 11,
+    "Peas": 12,
+    "Rice": 13,
+    "Watermelon": 14
+}
+
+# Create Flask app
 app = Flask(__name__)
 
 @app.route('/')
@@ -19,7 +35,7 @@ def index():
 @app.route("/predict", methods=['POST'])
 def predict():
     # Collect input values from the form
-    N = int(request.form.get('Nitrogen', 0))  # Default value if key is missing
+    N = int(request.form.get('Nitrogen', 0))
     P = int(request.form.get('Phosporus', 0))
     K = int(request.form.get('Potassium', 0))
     ph = float(request.form.get('pH', 0.0))
@@ -27,34 +43,17 @@ def predict():
     temperature = float(request.form.get('Temperature', 0.0))
     humidity = float(request.form.get('Humidity', 0.0))
 
+    # Create feature vector
     data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
-    my_prediction = model.predict(data)
-    final_prediction = my_prediction[0]
-
-    # Create a feature vector with numeric values
-    #feature_list = [N, P, K, temperature, humidity, ph, rainfall]
-    #single_pred = np.array(feature_list).reshape(1, -1)
 
     # Make prediction using the model
-    #prediction = model.predict(single_pred)
+    prediction_index = model.predict(data)[0]
 
-    # Map prediction to crop name
-    crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
-                 8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
-                 14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
-                 19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
+    # Get the corresponding crop name from the dictionary
+    predicted_crop = next((crop for crop, index in crop_indices.items() if index == prediction_index), "Unknown")
 
-    # Display result
-    """if prediction[0] in crop_dict:
-        crop = crop_dict[prediction[0]]
-        result = "{} is the best crop to be cultivated right there".format(crop)
-    else:
-        result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
-"""
-    return render_template('index.html', result=final_prediction)
+    # Return the predicted crop name to the template
+    return render_template('index.html', result=predicted_crop)
 
-
-
-# python main
 if __name__ == "__main__":
     app.run(debug=True)
